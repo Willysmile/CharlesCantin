aa9e13f6c9f4cd84275ea8cbdefe0201cd74d795
Willysmile <fabienrabillard@yahoo.fr>
2022-04-22 19:31:34 +0200

    Add Home background image & H1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./**/*.html"],
   theme: {
@@ -5,7 +7,21 @@ module.exports = {
       center: true,
     },
     extend: {
-      colors: {},
+      fontFamily: {
+        serif: [
+          "Nobile",
+          "Mukta Vaani",
+          "Cormorant Infant",
+          ...defaultTheme.fontFamily.serif,
+        ],
+      },
+      colors: {
+        primary_cc: "#47545F",
+        secondary_cc: "#AFDEDC",
+        white_cc: "#F0F7EE",
+        purple_cc: "#C4D7F2",
+        green_cc: "#91A8A4",
+      },
     },
   },
   variants: {},
