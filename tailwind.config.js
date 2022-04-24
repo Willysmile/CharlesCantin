38411a06301e70709ec87bb4c427e3a3c5e98768
Willysmile <fabienrabillard@yahoo.fr>
2022-04-24 17:17:35 +0200

    Add form

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,5 +25,5 @@ module.exports = {
     },
   },
   variants: {},
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
 };
